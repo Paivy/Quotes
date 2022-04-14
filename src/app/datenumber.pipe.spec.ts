0670bd09af5d73bd68ad44e28d34582c280b55df
Paivy <paivyeshirera@gmail.com>
2022-04-15 02:27:11 +0300

    initial commit

---
+++ src/app/datenumber.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DatenumberPipe } from './datenumber.pipe';
+
+describe('DatenumberPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DatenumberPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
